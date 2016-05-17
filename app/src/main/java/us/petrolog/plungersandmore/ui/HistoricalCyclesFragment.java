bb530b2cf1796c73cae03dcf93dd114462f9cc1d
Carlos Labrado <labrado.carlos@gmail.com>
2016-05-17 14:06:58 -0600

    Feat: Historical Graph Eye Candy

--- app/src/main/java/us/petrolog/plungersandmore/ui/HistoricalCyclesFragment.java
+++ app/src/main/java/us/petrolog/plungersandmore/ui/HistoricalCyclesFragment.java
@@ -1,10 +1,21 @@
 package us.petrolog.plungersandmore.ui;
 
+import android.animation.PropertyValuesHolder;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.db.chart.Tools;
+import com.db.chart.model.LineSet;
+import com.db.chart.view.AxisController;
+import com.db.chart.view.LineChartView;
+import com.db.chart.view.Tooltip;
+import com.db.chart.view.animation.Animation;
+import com.db.chart.view.animation.easing.BounceEase;
 
 import us.petrolog.plungersandmore.R;
 
@@ -22,6 +33,17 @@ public class HistoricalCyclesFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    private LineChartView mChart;
+
+
+    private final String[] mLabels = {"Jan", "Fev", "Mar", "Apr", "Jun", "May", "Jul", "Aug", "Sep"};
+    private final float[][] mValues = {{1120f, 1210f, 1212f, 1230f, 1120f, 1250f, 1300f, 1280f, 1200f},
+            {1200f, 1000f, 1120f, 1300f, 1230f, 1250f, 1112f, 1146f, 1234f}};
+
+    private Tooltip mTip;
+
 
     public HistoricalCyclesFragment() {
         // Required empty public constructor
@@ -58,7 +80,81 @@ public class HistoricalCyclesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historical_cycles, container, false);
+        View view = inflater.inflate(R.layout.fragment_historical_cycles, container, false);
+
+        mChart = (LineChartView) view.findViewById(R.id.chart1);
+
+        showCard();
+
+        return view;
+    }
+
+    private void showCard() {
+        // Tooltip
+        mTip = new Tooltip(getContext(), R.layout.linechart_three_tooltip, R.id.value);
+
+//        ((TextView) mTip.findViewById(R.id.value))
+//                .setTypeface(Typeface.createFromAsset(getContext().getAssets(), "OpenSans-Semibold.ttf"));
+
+        mTip.setVerticalAlignment(Tooltip.Alignment.BOTTOM_TOP);
+        mTip.setDimensions((int) Tools.fromDpToPx(100), (int) Tools.fromDpToPx(25));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+
+            mTip.setEnterAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 1),
+                    PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f),
+                    PropertyValuesHolder.ofFloat(View.SCALE_X, 1f)).setDuration(200);
+
+            mTip.setExitAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 0),
+                    PropertyValuesHolder.ofFloat(View.SCALE_Y, 0f),
+                    PropertyValuesHolder.ofFloat(View.SCALE_X, 0f)).setDuration(200);
+
+            mTip.setPivotX(Tools.fromDpToPx(65) / 2);
+            mTip.setPivotY(Tools.fromDpToPx(25));
+        }
+
+        mChart.setTooltips(mTip);
+
+        // Data
+        LineSet dataset = new LineSet(mLabels, mValues[0]);
+        dataset.setColor(Color.parseColor("#758cbb"))
+                .setFill(Color.parseColor("#2d374c"))
+                .setDotsColor(Color.parseColor("#758cbb"))
+                .setThickness(4)
+                .setDashed(new float[]{10f, 10f})
+                .beginAt(5);
+        mChart.addData(dataset);
+
+        dataset = new LineSet(mLabels, mValues[0]);
+        dataset.setColor(Color.parseColor("#b3b5bb"))
+                .setFill(Color.parseColor("#2d374c"))
+                .setDotsColor(Color.parseColor("#ffc755"))
+                .setThickness(4)
+                .endAt(6);
+        mChart.addData(dataset);
+
+        // Chart
+        mChart.setBorderSpacing(Tools.fromDpToPx(15))
+                .setAxisBorderValues(1000, 1400)
+                .setYLabels(AxisController.LabelPosition.NONE)
+                .setLabelsColor(Color.parseColor("#6a84c3"))
+                .setXAxis(false)
+                .setYAxis(false);
+
+        Runnable chartAction = new Runnable() {
+            @Override
+            public void run() {
+                mTip.prepare(mChart.getEntriesArea(0).get(3), mValues[0][3]);
+                mChart.showTooltip(mTip, true);
+            }
+        };
+
+        Animation anim = new Animation()
+                .setEasing(new BounceEase())
+                .setEndAction(chartAction);
+
+        mChart.show(anim);
+
     }
 
 }
